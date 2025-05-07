3b951760eca792eb70e4c2528aec970966f19a72
Resya-Hasan <resyahasan0@gmail.com>
2025-05-07 14:26:45 +0700

    setup socket server side #2

--- app.js
+++ app.js
@@ -9,21 +9,47 @@ const { Server } = require('socket.io')
 const app = express()
 const port = 3000
 
-const server = createServer(app)
-const io = new Server(server)
-
 const cors = require('cors')
+const server = createServer(app)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    },
+})
+
+const chats = [
+    {
+        messages: 'Hello World!',
+    }
+]
+
 app.use('/', require('./routers'))
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
+    console.log('a user connected', socket.id)
+
+    socket.on('messages.get', () => {
+        console.log('messages.get >>>>')
+        socket.emit('messages.data', chats)
+    })
+
+    socket.on('sendMessage', (data) => {
+        console.log('sendMessage >>>>', data)
+        chats.push({messages: data.message})
+        console.log('chats >>>>', chats)
+        io.emit('messages.data', chats)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected', socket.id)
+    })
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
