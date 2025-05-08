949c450365929233f5b9200274e708e83389baaa
ellayulia15 <ella.yuliacahyani@gmail.com>
2025-05-08 10:53:34 +0700

    Add API Docs

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "*",
         credentials: true,
     },
 });
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
